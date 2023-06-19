55a591627648c32effbad9d93736c8912c5d19e0
611619104109 <venkatvenkatesan2905@gmail.com>
2023-06-19 09:46:43 +0530

    first commit

--- src/About.js
+++ src/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
@@ -6,7 +7,7 @@ const About = () => {
       <h1>About</h1>
       <p style={{marginTop:"2rem"}}> 
       We are Kickinsta team we develope this app for sharing your knowledge to others,
-      if you face any querie,contact us
+      if you face any querie,contact use Email:<Link to="https://mail.google.com/mail/u/0/?tab=rm&ogbl">kickinsta@gmail.com</Link>
       </p>
     </main>
   )
